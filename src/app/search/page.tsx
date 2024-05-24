98ddb05b786787717dc4509d90959f825c2236a6
Piyush <piyushsvps@gmail.com>
2024-05-24 16:59:15 +0530

    add search box

---
+++ src/app/search/page.tsx
@@ -0,0 +1,33 @@
+import Results from "@/components/Results";
+import React from "react";
+
+interface SearchPageProps {
+  searchParams: {
+    q: string;
+  };
+}
+
+const SearchPage = async ({ searchParams }: SearchPageProps) => {
+  const { q } = searchParams;
+  const res = await fetch(
+    `https://api.themoviedb.org/3/search/multi?api_key=${process.env.API_KEY}&query=${q}`
+  );
+
+  const data = await res.json();
+  const results = data.results;
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  return (
+    <div>
+      {results && results.length === 0 && (
+        <h1 className="text-center pt-6">No results found</h1>
+      )}
+      {results && <Results results={results} />}
+    </div>
+  );
+};
+
+export default SearchPage;
