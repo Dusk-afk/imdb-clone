0f547b754e78fe37814bf9c5bbb2f341e40a7ef9
Piyush <piyushsvps@gmail.com>
2024-06-27 19:17:57 +0530

    Added suspense

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import Header from "@/components/Header";
 import Providers from "./Providers";
 import Navbar from "@/components/Navbar";
 import SearchBox from "@/components/SearchBox";
+import { Suspense } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,7 +24,9 @@ export default function RootLayout({
       <body className={inter.className}>
         <Providers>
           <Header />
-          <Navbar />
+          <Suspense>
+            <Navbar />
+          </Suspense>
           <SearchBox />
           {children}
         </Providers>
